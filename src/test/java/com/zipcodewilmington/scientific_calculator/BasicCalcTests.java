bb6c3fd40eaac73814de15a188382fdbe9a30fb5
aash-bjw <50807447+aash-bjw@users.noreply.github.com>
2019-10-18 17:22:36 -0400

    Updated basicCalc tests

--- src/test/java/com/zipcodewilmington/scientific_calculator/BasicCalcTests.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/BasicCalcTests.java
@@ -63,12 +63,103 @@ public class BasicCalcTests {
     }
 
     @Test
-    public void multiplyTest(){
+    public void subtractTest2(){
+
+        double x = 50;
+        double y = 5.5;
+
+        double expected = 44.5;
+        double actual = calc.subtract(x,y);
+
+        assertEquals(expected, actual);
 
     }
 
     @Test
-    public void divideTest(){
+    public void subtractTest3(){
+
+        double x = 100;
+        double y = 7.65;
+
+        double expected = 92.35;
+        double actual = calc.subtract(x,y);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void multiplyTest1(){
+
+        double x = 40;
+        double y = 4;
+
+        double expected = 160;
+        double actual = calc.multiply(x,y);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void multiplyTest2(){
+
+        double x = 7.5;
+        double y = 4.4;
+
+        double expected = 33;
+        double actual = calc.multiply(x,y);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void multiplyTest3(){
+
+        double x = 800;
+        double y = 6.6;
+
+        double expected = 5280;
+        double actual = calc.multiply(x,y);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void divideTest1(){
+        double x = 865;
+        double y = 5;
+
+        double expected = 173;
+        double actual = calc.divide(x,y);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void divideTest2(){
+        double x = 740;
+        double y = 10;
+
+        double expected = 74;
+        double actual = calc.divide(x,y);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void divideTest3(){
+        double x = 80;
+        double y = 2;
+
+        double expected = 40;
+        double actual = calc.divide(x,y);
+
+        assertEquals(expected, actual);
+
 
     }
 }
